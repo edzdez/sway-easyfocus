786c579fb162b998c3a8c259bdfa3ab715e5217c
Ethan Zhang <ezsland05@gmail.com>
2023-07-07 10:27:06 -0700

    gtk starter

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
+use gtk::prelude::*;
+use gtk::{Application, ApplicationWindow};
+
 fn main() {
-    println!("Hello, world!");
+    let app = Application::builder()
+        .application_id("org.example.HelloWorld")
+        .build();
+
+    app.connect_activate(|app| {
+        // We create the main window.
+        let win = ApplicationWindow::builder()
+            .application(app)
+            .default_width(320)
+            .default_height(200)
+            .title("Hello, World!")
+            .build();
+
+        // Don't forget to make all widgets visible.
+        win.show_all();
+    });
+
+    app.run();
 }
