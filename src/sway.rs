ce3842918a9e2263869bc2b4dacffa8ad614fe92
Mark Stosberg <mark@stosberg.com>
2023-11-04 20:54:24 -0400

    WIP: multi-monitor support
     * Details about each output are collected, windows and drawn and
       keypresses are handled.
     * Character hints are unique across outputs.
    
     * All the overlays are drawn on the same output instead instead of
       diferrent ones.
     * Pressing a key is only closing the hints on a single output. So, if
       if you have three monitors, it takes three key presses to exit.
    
     * I couldn't figure out the Rust memory managent to pass around the
       Sway IPC connection through a "for" loop with closures. So in
       sway::focus(), I just re-connected. That solved the problem and
       I can't tell a noticable difference in performance, but maybe
       there's a better way.
    
     * I had difficulty locating the Rust GTK docs for how to open an
       overlay window on a different output and am pausing for now.
    
    Feel free to pick up the branch and keep working on it.

--- src/sway.rs
+++ src/sway.rs
@@ -16,11 +16,29 @@ pub fn get_tree(conn: Arc<Mutex<Connection>>) -> Node {
         .expect("failed to communicate with sway")
 }
 
-pub fn get_focused_output(conn: Arc<Mutex<Connection>>) -> Node {
+// All output nodes, focused or not
+pub fn get_all_output_nodes(conn: Arc<Mutex<Connection>>) -> Vec<Node> {
+    let mut output_nodes = vec![];
+    let mut q = VecDeque::new();
     let root_node = get_tree(conn);
-    root_node
-        .find_focused(|n| n.node_type == swayipc::NodeType::Output)
-        .expect("could not find focused output")
+
+    q.push_back(root_node);
+
+    while !q.is_empty() {
+        // We can unwrap because we know the queue is not empty
+        let node = q.pop_back().unwrap();
+
+        // If we hav an output node
+        if (node.node_type == NodeType::Output) && !node.nodes.is_empty() {
+            output_nodes.push(node.clone());
+        }
+
+        // Look for more outputs in the children
+        for child in node.nodes {
+            q.push_back(child.clone());
+        }
+    }
+    output_nodes
 }
 
 pub fn get_focused_workspace(output: &Node) -> Node {
@@ -30,7 +48,7 @@ pub fn get_focused_workspace(output: &Node) -> Node {
         .expect("could not find focused workspace")
 }
 
-pub fn get_all_windows(workspace: &Node) -> Vec<Node> {
+pub fn get_window_nodes_for_one_workspace(workspace: &Node) -> Vec<Node> {
     let mut nodes = vec![];
 
     let mut q = VecDeque::new();
@@ -52,7 +70,7 @@ pub fn get_all_windows(workspace: &Node) -> Vec<Node> {
         }
 
         /*
-        // floating nodes
+        // TODO: floating nodes
         for child in node.floating_nodes {
             q.push_back(child.clone());
         }
@@ -63,9 +81,8 @@ pub fn get_all_windows(workspace: &Node) -> Vec<Node> {
     nodes
 }
 
-pub fn focus(conn: Arc<Mutex<Connection>>, con_id: i64) {
-    let mut conn_lock = conn.lock().unwrap();
-    conn_lock
+pub fn focus(con_id: i64) {
+    acquire_connection()
         .run_command(format!("[con_id={}] focus", con_id))
         .expect("failed to focus container");
 }
